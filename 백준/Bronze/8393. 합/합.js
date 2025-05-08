af16230518ab728f229f3a4ca85caa1ab270bcc8
Sujin Oh <osj1405@naver.com>
2025-05-08 10:59:35 +0900

    [Bronze V] Title: 합, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,10 +1,7 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let number = Number(input[0]);
-let sum = 0;
 
-for(i = 0; i < number; i++){
-    sum += i + 1;
-}
+let sum = parseInt((1 + number) * number / 2);
 
 console.log(sum);
