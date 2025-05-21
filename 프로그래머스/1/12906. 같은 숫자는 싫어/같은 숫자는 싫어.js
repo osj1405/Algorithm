119170a973a0d29d9cf5b05506cd4b513e817507
Sujin Oh <osj1405@naver.com>
2025-05-21 15:50:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.87 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+function solution(arr)
+{
+    var answer = [];
+    for(let i = 0; i < arr.length; i++){
+        if(answer.length > 0 && answer[answer.length - 1] == arr[i]){
+            continue;
+        } else{
+            answer.push(arr[i]);
+        }
+    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log(answer)
+    
+    return answer;
+}
