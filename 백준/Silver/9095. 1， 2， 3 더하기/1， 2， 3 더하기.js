ca7781ce5b453ed203e72189202cc7558088f234
Sujin Oh <osj1405@naver.com>
2026-04-15 11:07:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,40 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let T = Number(input[0])
+
+function sumDp(number){
+    let count = 0
+    
+    return count
+}
+
+let result = []
+
+for(let i = 1; i <= T; i++){
+    let number = Number(input[i])
+    
+    let dp = Array(number + 1).fill(0)
+    
+    dp[0] = 1
+    
+    for(let j = 1; j <= number; j++){
+        if(j === 1) {
+            dp[j] = dp[0]
+            continue
+        }
+        if(j === 2) {
+            dp[j] = dp[j - 1] + dp[0]
+            continue
+        }
+        if(j === 3){
+            dp[j] = dp[j - 1] + dp[j - 2] + dp[0]
+            continue
+        }
+        dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3]
+    }
+    
+    result.push(dp[number])
+}
+
+console.log(result.join('\n'))
